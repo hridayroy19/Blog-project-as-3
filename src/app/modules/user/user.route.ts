4e92b4d705ebd9a1d7a06eefbc50e55c15073f41
hridayroy19 <hrhridoyroy503@gmail.com>
2024-12-19 20:49:02 +0600

    crete user validation sechma

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { userController } from './user.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { userValidation } from './userValidation';
 
 const userRouter = Router();
 
-userRouter.post('/user/creat-user', userController.creatUser);
+userRouter.post('/user/creat-user',validateRequest(userValidation.userValidationZod),userController.creatUser);
 userRouter.post('/user/');
 
 export default userRouter;
---
+++ src/app/modules/user/userValidation.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+
+const userValidationZod = z.object({
+    name: z.string().nonempty("Name is required"),
+    email: z.string().email("Invalid email format"),
+    password: z.number().min(6, "Password must be at least 6 characters long"),
+    role: z.enum(['admin', 'user']).optional().default('user'),
+    isBlocked: z.boolean().optional().default(false),
+})
+
+export const userValidation = {
+    userValidationZod
+}
